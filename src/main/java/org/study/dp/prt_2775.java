fd5c1313dc66ca90b53e1a1a9380da3cf9d1e0f6
jy0025 <jy0025@soleo.kr>
2023-12-07 18:37:50 +0900

    백준 2775 풀이(미완)

--- src/main/java/org/study/dp/prt_2775.java
+++ src/main/java/org/study/dp/prt_2775.java
@@ -5,36 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class prt_2775 {
-    static int[][] arr;
     static int result = 0;
     static boolean flag = true;
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[15][15];
         int t = Integer.parseInt(br.readLine());
-//        for(int i = 0; i < t; i++){
+        for (int i = 0; i < t; i++) {
             int k = Integer.parseInt(br.readLine()); //층
             int n = Integer.parseInt(br.readLine()); //호
-            arr = new int[k+1][n];
-            dp(k, n); //1층 이상
-        }
-//    }
-
-    private static int dp(int k, int n) {
-       //1층 끝나면 다음층부터 각 호수를 더해줌
-        for(int x = k-1; x <= n; x++){ //층
-            int tmp = 0;
-            for(int y = 0; y < n; y++){ //호수
-                if(flag){
-                    arr[x][y] = y+1;
-                }else{
-                  tmp = arr[x-x][y];
-                   result += dp(x-x, n-1) + tmp; //시작
-
+            for(int x = 0; x<= 15; x++){
+            for (int y = 0; y < 15; y++) {
+                arr[x][y] = y + 1; //0층 주민
+                if (x >= 1) {
+                    arr[x][y] = arr[x - 1][y] + arr[x - 1][y + x];
                 }
             }
-            flag = false;
+            }
         }
-        return result;
     }
+
 }
